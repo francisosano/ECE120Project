0e16e30ad0e07ab2a2f4101d0d2b11e413cc6451
jjjest <jestjoyce.tagalogon@msugensan.edu.ph>
2022-01-17 04:58:48 +0800

    code

--- dsp.py
+++ dsp.py
@@ -2,3 +2,15 @@ import numpy as np
 import wave
 import struct
 import matplotlib.pyplot as plt
+
+def note_detect(wav_file):
+    wav_file=
+    #get the frame rate
+    #get the sampling frequency
+    file_length=wav_file.getnframes()
+    f_s=wav_file.getframerate()
+    Sound=np.zeros(file_length)
+
+    print(file_length)
+    print(f_s)
+    print(Sound)
